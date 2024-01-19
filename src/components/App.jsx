efa754e374fe7e2a2290a002234ae4f8bc8d16c0
Cris8791 <135622871+Cris8791@users.noreply.github.com>
2024-01-19 17:26:27 +0200

    commit_transactions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,8 @@ import Statistics from './statistics/Statistics';
 import statsData from './statistics/data.json';
 import FriendList from './friendList/FriendList';
 import friends from './friendList/friends.json';
+import TransactionHistory from './transactionHistory/TransactionHistory';
+import transactions from './transactionHistory/transactions.json';
 
 export const App = () => {
   return (
@@ -20,6 +22,8 @@ export const App = () => {
       <Statistics title="Upload stats" stats={statsData} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
